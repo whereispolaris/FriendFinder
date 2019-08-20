2c02cbc46f17fd778759c9cc53227f307a8f3fa2
Santiago Enciso <whereispolaris@gmail.com>
2019-08-19 21:36:49 -0500

    Set up homepage route and started the HTTP server listening for connections.

--- server.js
+++ server.js
@@ -1,1 +1,14 @@
 // File that controls all the backend activity. Contains express and path modules.
+
+const express = require('express')
+const app = express()
+const PORT = process.env.PORT || 3000;
+
+// respond with "hello world" when a GET request is made to the homepage
+app.get('/', function (req, res) {
+    res.send('hello world')
+});
+
+app.listen(PORT, () => {
+    console.log("App listening in Port: " + PORT);
+});
