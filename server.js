9bd6969a1e1d2404504fcdcc67d90267464f55f2
Santiago Enciso <whereispolaris@gmail.com>
2019-08-21 15:36:54 -0500

    Added body parser npm module.

--- server.js
+++ server.js
@@ -1,13 +1,19 @@
 // File that controls all the backend activity. Contains express and path modules.
-
+const bodyParser = require("body-parser");
 const express = require('express')
-const app = express()
+const app = express();
 const PORT = process.env.PORT || 3000;
 
 // respond with "hello world" when a GET request is made to the homepage
 app.get('/', function (req, res) {
     res.send('hello world')
 });
+
+// parse application/x-www-form-url-encoded
+app.use(bodyParser.urlencoded({ extended: false }));
+
+// parse application/json
+app.use(bodyParser.json());
 
 app.listen(PORT, () => {
     console.log("App listening in Port: " + PORT);
