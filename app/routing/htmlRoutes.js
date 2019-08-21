1c88f5e7fe9e3c26ea40c53a05b4b58412d4968e
Santiago Enciso <whereispolaris@gmail.com>
2019-08-21 15:57:55 -0500

    Added HTML routes for home and survey files.

--- app/routing/htmlRoutes.js
+++ app/routing/htmlRoutes.js
@@ -2,3 +2,18 @@
 
 // A GET Route to / survey which should display the survey page.
 // A default, catch-all route that leads to home.html which displays the home page.
+
+// Require path
+var path = require('path');
+
+module.exports = function (app) {
+    app.get('/survey', function (req, res) {
+        res.sendFile(path.join(__dirname + '../public/survey.html'));
+    });
+
+    // homepage or any other page
+    app.use(function (req, res) {
+        res.sendFile(path.join(__dirname + '../public/home.html'));
+    });
+
+}
