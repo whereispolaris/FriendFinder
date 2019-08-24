981fdff713594cea9f3bea002dceaac9a614fc8e
Santiago Enciso <whereispolaris@gmail.com>
2019-08-24 18:27:03 -0500

    Updated static frient data to get better test results.

--- app/data/friend.js
+++ app/data/friend.js
@@ -2,25 +2,25 @@
 
 friendData = [
     {
-        "name": "Santiago",
+        "name": "Santiago Enciso",
         "photo": "https://media.licdn.com/dms/image/C4E03AQFV-QmxJ7hiDw/profile-displayphoto-shrink_200_200/0?e=1571875200&v=beta&t=zUxrOB9pLSLZCQcd-UWtIZE3WNsDEqumvAAi-RQNaWw",
         "scores": [
             5,
-            1,
-            4,
-            4,
             5,
-            1,
-            2,
             5,
-            4,
-            4,
-            1
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5,
+            5
         ]
     },
     {
-        "name": "Anthony",
-        "photo": "https://picsum.photos/id/33/300/300.jpg",
+        "name": "Anthony ",
+        "photo": "https://images.pexels.com/photos/555790/pexels-photo-555790.png?auto=compress&cs=tinysrgb&dpr=1&w=300",
         "scores": [
             3,
             3,
@@ -36,20 +36,37 @@ friendData = [
         ]
     },
     {
-        "name": "Johny",
-        "photo": "https://picsum.photos/id/33/300/300.jpg",
+        "name": "Johny James",
+        "photo": "https://s7d2.scene7.com/is/image/FoxRacing/profile_MikeMason_2?$dw_riderprofile$",
         "scores": [
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5,
-            5
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2,
+            2
+        ]
+    },
+    {
+        "name": "Michelle Obama",
+        "photo": "https://media.self.com/photos/5be5c2122a9bce53e643ff73/4:3/w_728,c_limit/GettyImages-629949330.jpg",
+        "scores": [
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1,
+            1
         ]
     }
 ]
